14d1a4c2c0830651bf537e2641d38a88ca0adcea
Manoel Campos <manoelcampos@gmail.com>
2023-06-01 23:54:13 -0300

    Refactoring
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/com/manoelcampos/randomorg/Result.java
+++ src/main/java/com/manoelcampos/randomorg/Result.java
@@ -7,10 +7,10 @@ import lombok.Setter;
  *  @author Manoel Campos da Silva Filho
  */
 @Getter @Setter
-public class Result {
-    private RandomData random;
+class Result {
     private long bitsUsed;
     private long bitsLeft;
     private int requestsLeft;
     private int advisoryDelay;
+    private RandomData random;
 }
