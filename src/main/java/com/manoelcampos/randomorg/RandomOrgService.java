10cd2c09b4a3f136d6f924bae812728669d13a74
Manoel Campos <manoelcampos@gmail.com>
2023-06-01 23:56:00 -0300

    Update docs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/com/manoelcampos/randomorg/RandomOrgService.java
+++ src/main/java/com/manoelcampos/randomorg/RandomOrgService.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
- * Client to https://random.org service.
+ * Client to <a href="https://random.org">https://random.org</a> service.
  * @author Manoel Campos da Silva Filho
  * @see <a href="https://api.random.org/json-rpc/4/basic">API Docs</a>
  */
